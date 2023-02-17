122a2958510c4751c5a85a5fe2322fb8a8ca993a
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-02-16 23:20:27 -0300

    Adicionado teste de integração temporário para a rota /eventos/diaDoAno

--- __test__/integration/app.test.js
+++ __test__/integration/app.test.js
@@ -17,4 +17,11 @@ describe("Testes de integração", () => {
         expect(response.body).toStrictEqual([]);
     });
 
+    it("GET para /eventos/diaDoAno", async () => {
+        const response = await superTest(app)
+            .get("/api/v1/eventos/diaDoAno/1");
+
+        expect(response.body).toStrictEqual([]);
+    });
+
 });
