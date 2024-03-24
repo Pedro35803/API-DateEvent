04c4b1dac122dff73c5d8094f6c360d4e51b5902
Pedro <pedrojuniorifpb@gmail.com>
2024-03-24 15:05:41 -0300

    Reovendo código de teste do sequelize

--- src/database/models/ForeignKey/Event.js
+++ src/database/models/ForeignKey/Event.js
@@ -26,30 +26,6 @@ const init = async () => {
   await Event.sync();
   await EventStatic.sync();
   await EventDynamic.sync();
-
-  const eventCreate = await Event.create({
-    name: "Dia do futebol",
-    type: "Evento Comum",
-    isDynamic: false,
-  });
-
-  await EventDynamic.create({
-    idEvent: eventCreate.id,
-    date: "2022-07-19",
-  });
-
-  const event = await Event.findOne({
-    where: { id: eventCreate.id },
-    include: [
-      { model: EventDynamic },
-      { model: EventStatic },
-    ],
-    raw: true,
-    nest: true
-  });
-
-  // const { date } = event.dataValues.eventDynamic.dataValues
-
-  console.log(event);
 };
+
 init();
